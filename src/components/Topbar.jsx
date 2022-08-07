b749cfd49eb95dd470976e08371b223925867643
Rain981012 <siyuan9@ualberta.ca>
2022-08-07 01:57:45 -0400

    new

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,18 @@
+import { Card, CardMedia } from "@mui/material";
+import React from "react";
+import background_sample from "../test_picture/Background.jpg";
+
+export const Topbar = () => {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        height="255"
+        image={background_sample}
+        alt="Paella dish"
+      />
+    </Card>
+  );
+};
+
+export default Topbar;
